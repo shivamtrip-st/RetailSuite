d4892ff6c84190d42527aaab3f34e45d3758f25f
shivamtrip-st <shivamtripqthist@gmail.com>
2025-05-04 11:38:46 +0530

    [ADD] Initial commit: Added RetailSuite - a Django-based e-commerce platform with inventory and admin panel

--- store/static/store/js/app.js
+++ store/static/store/js/app.js
@@ -156,3 +156,13 @@ $(document).ready(function() {
     });
 
 });
+
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+            var toastList = toastElList.map(function (toastEl) {
+                var toast = new bootstrap.Toast(toastEl, {
+                    autohide: true,
+                    delay: 5000
+                })
+                toast.show()
+                return toast
+            })
